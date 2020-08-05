19ab87dce3312ce612fc7f950ba98e31a0316a68
skyge <18844062006@163.com>
2020-08-05 15:33:33 +0800

    A Add staking contracts.

---
+++ README.md
@@ -0,0 +1,30 @@
+## dForce (DF) Liquidity Mining
+
+### Mainnet Contract Address(2020-08-03)
+
+<table>
+	<tr>
+        <th>Contract Name</th>
+    	<th>Contract Address</th>
+	</tr>
+	<tr>
+		<td> USDx-DF Staking </td>
+		<td> 0x5e84fC41D3aDd07A34616F781DCF1e49e8DC41C1 </td>
+	</tr>
+	<tr>
+		<td> GOLDx-DF Staking </td>
+		<td> 0xdC7A844a45Ef936497FB916f1c2Ddb80F59a8aDc </td>
+	</tr>
+	<tr>
+		<td> dDAI Staking </td>
+		<td> 0xD2fA07cD6Cd4A5A96aa86BacfA6E50bB3aaDBA8B </td>
+	</tr>
+	<tr>
+		<td> dUSDC Staking </td>
+		<td> 0xB71dEFDd6240c45746EC58314a01dd6D833fD3b5 </td>
+	</tr>
+	<tr>
+		<td> dUSDT Staking </td>
+		<td> 0x324EebDAa45829c6A8eE903aFBc7B61AF48538df </td>
+	</tr>
+</table>
---
+++ contracts/staking-audit.sol
@@ -0,0 +1,149 @@
+pragma solidity ^0.5.0;
+
+import "@openzeppelin/contracts/math/Math.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+
+
+contract IRewardDistributionRecipient is Ownable {
+    address rewardDistribution;
+
+    function notifyRewardAmount(uint256 reward) external;
+
+    modifier onlyRewardDistribution() {
+        require(_msgSender() == rewardDistribution, "Caller is not reward distribution");
+        _;
+    }
+
+    function setRewardDistribution(address _rewardDistribution)
+        external
+        onlyOwner
+    {
+        rewardDistribution = _rewardDistribution;
+    }
+}
+
+
+contract LPTokenWrapper {
+
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    IERC20 public lp = IERC20(0xe9Cf7887b93150D4F2Da7dFc6D502B216438F244);
+
+    uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
+
+    function totalSupply() public view returns(uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view returns(uint256) {
+        return _balances[account];
+    }
+
+    function stake(uint256 amount) public {
+        _totalSupply = _totalSupply.add(amount);
+        _balances[msg.sender] = _balances[msg.sender].add(amount);
+        lp.safeTransferFrom(msg.sender, address(this), amount);
+    }
+
+    function withdraw(uint256 amount) public {
+        _totalSupply = _totalSupply.sub(amount);
+        _balances[msg.sender] = _balances[msg.sender].sub(amount);
+        lp.safeTransfer(msg.sender, amount);
+    }
+}
+
+
+contract Unipool is LPTokenWrapper, IRewardDistributionRecipient {
+
+    IERC20 public df = IERC20(0x431ad2ff6a9C365805eBaD47Ee021148d6f7DBe0);
+
+    uint256 public constant DURATION = 7 days;
+
+    uint256 public periodFinish = 0;
+    uint256 public rewardRate = 0;
+    uint256 public lastUpdateTime;
+    uint256 public rewardPerTokenStored;
+    mapping(address => uint256) public userRewardPerTokenPaid;
+    mapping(address => uint256) public rewards;
+
+    event RewardAdded(uint256 reward);
+    event Staked(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+    event RewardPaid(address indexed user, uint256 reward);
+
+    modifier updateReward(address account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = lastTimeRewardApplicable();
+        if (account != address(0)) {
+            rewards[account] = earned(account);
+            userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        }
+        _;
+    }
+
+    function lastTimeRewardApplicable() public view returns(uint256) {
+        return Math.min(block.timestamp, periodFinish);
+    }
+
+    function rewardPerToken() public view returns(uint256) {
+        if (totalSupply() == 0) {
+            return rewardPerTokenStored;
+        }
+        return rewardPerTokenStored.add(
+            lastTimeRewardApplicable().sub(lastUpdateTime).mul(rewardRate).mul(1e18).div(totalSupply())
+        );
+    }
+
+    function earned(address account) public view returns(uint256) {
+        return balanceOf(account).mul(
+            rewardPerToken().sub(userRewardPerTokenPaid[account])
+        ).div(1e18).add(rewards[account]);
+    }
+
+    function stake(uint256 amount) public updateReward(msg.sender) {
+        require(amount > 0, "Cannot stake 0");
+        super.stake(amount);
+        emit Staked(msg.sender, amount);
+    }
+
+    function withdraw(uint256 amount) public updateReward(msg.sender) {
+        require(amount > 0, "Cannot withdraw 0");
+        super.withdraw(amount);
+        emit Withdrawn(msg.sender, amount);
+    }
+
+    function exit() public {
+        withdraw(balanceOf(msg.sender));
+        getReward();
+    }
+
+    function getReward() public updateReward(msg.sender) {
+        uint256 reward = earned(msg.sender);
+        if (reward > 0) {
+            rewards[msg.sender] = 0;
+            df.safeTransfer(msg.sender, reward);
+            emit RewardPaid(msg.sender, reward);
+        }
+    }
+
+    function notifyRewardAmount(uint256 reward) external onlyRewardDistribution updateReward(address(0)) {
+        if (block.timestamp >= periodFinish) {
+            rewardRate = reward.div(DURATION);
+        } else {
+            uint256 remaining = periodFinish.sub(block.timestamp);
+            uint256 leftover = remaining.mul(rewardRate);
+            rewardRate = reward.add(leftover).div(DURATION);
+        }
+        lastUpdateTime = block.timestamp;
+        periodFinish = block.timestamp.add(DURATION);
+        emit RewardAdded(reward);
+    }
+
+    function lockedDetails() external view returns (bool, uint256) {
+        return (false, periodFinish);
+    }
+}
